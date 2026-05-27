c1d7fd63e3e0b5ab5daf9747e7ec5db9b9386621
Rajat Kumar <grajatkumar54321@gmail.com>
2026-05-27 11:51:17 +0530

    for loop basic

---
+++ src/for_loop_1.java
@@ -0,0 +1,15 @@
+//print hello world n times Take n as input from user
+import java.util.Scanner;
+public class for_loop_1 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("enter value of n : ");
+        int n = sc.nextInt();
+        int i;
+
+        for(i=1; i <= n; i++){
+            System.out.println(i + ". Hello world" );
+        }
+
+    }
+}
