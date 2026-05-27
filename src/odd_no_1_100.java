610d5ce9b9993238762b90eeb966f3d1319e976f
Rajat Kumar <grajatkumar54321@gmail.com>
2026-05-27 12:34:10 +0530

    for loop odd no 1-100

---
+++ src/odd_no_1_100.java
@@ -0,0 +1,11 @@
+public class odd_no_1_100 {
+    public static void main(String[] args) {
+        int i;
+        for(i=1; i<=100;i++){
+            if(i % 2 !=0 ){
+
+                System.out.println(i);
+            }
+        }
+    }
+}
