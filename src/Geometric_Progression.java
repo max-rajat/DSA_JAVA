0ef4c135ffa3934c982fde6799c898cda18cce80
Rajat Kumar <grajatkumar54321@gmail.com>
2026-05-27 13:38:26 +0530

    GP

---
+++ src/Geometric_Progression.java
@@ -0,0 +1,15 @@
+// display GP-1,2,4,8,16.... upto 'n' terms
+import java.util.Scanner;
+public class Geometric_Progression {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter n term : ");
+        int n = sc.nextInt();
+        int i;
+        int a=1;
+        for(i=1;i<=n;i++){
+            System.out.println(a);
+            a*=2;
+        }
+    }
+}
