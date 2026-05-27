0576c14a68c362cb848d7c7c67db594f52bb265e
Rajat Kumar <grajatkumar54321@gmail.com>
2026-05-27 12:26:11 +0530

    fro loop even no b/w 1-100

---
+++ src/even_no1_100.java
@@ -0,0 +1,11 @@
+// even no between 1-100
+public class even_no1_100 {
+    public static void main(String[] args) {
+        int i;
+        for(i=1;i<=100;i++){
+            if(i % 2 == 0){
+                System.out.println(i);
+            }
+        }
+    }
+}
