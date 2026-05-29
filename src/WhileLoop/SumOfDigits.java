9e09169c73ffc3758d38ac53d6499159b0331ea2
Rajat Kumar <grajatkumar54321@gmail.com>
2026-05-29 11:30:08 +0530

    SumofDigits

---
+++ src/WhileLoop/SumOfDigits.java
@@ -0,0 +1,17 @@
+package WhileLoop;
+import java.util.Scanner;
+public class SumOfDigits {
+    public static void main(String[] args) {
+        Scanner sc =new Scanner(System.in);
+        System.out.printf("Enter number");
+        int n = sc.nextInt();
+
+        int sum=0;
+         while(n!=0){
+             sum+=(n%10);
+             n/=10;
+         }
+        System.out.println(sum);
+    }
+
+}
