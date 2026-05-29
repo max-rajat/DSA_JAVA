0afe49f1db2315d2abde1ee5cdb957052e0f8b05
Rajat Kumar <grajatkumar54321@gmail.com>
2026-05-29 10:40:30 +0530

    while loop

---
+++ src/WhileLoop/print_1to10.java
@@ -0,0 +1,12 @@
+package WhileLoop;
+import java.util.Scanner;
+public class print_1to10 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int i=1;
+        while(i<=10){
+            System.out.println(i);
+            i++;
+        }
+    }
+}
