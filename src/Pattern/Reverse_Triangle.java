0e49702f685f475518865da716d990d8bb46e369
Rajat Kumar <grajatkumar54321@gmail.com>
2026-06-08 10:55:15 +0530

    Patterns

--- src/Pattern/Reverse_Triangle.java
+++ src/Pattern/Reverse_Triangle.java
@@ -9,8 +9,11 @@ public class Reverse_Triangle {
         for(i=1;i<=n;i++){
             for(j=1;j<=n+1-i;j++){
                 System.out.printf("*");
-            }
-            System.out.println();
-        }
+                    }
+                    System.out.println();
+                }
     }
 }
+
+
+
