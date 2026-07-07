7156ebcbcd9acc291e93383c7de536bb619dd62d
Rajat Kumar <grajatkumar54321@gmail.com>
2026-07-07 18:38:45 +0530

    Floyd's trangle

---
+++ src/Pattern/Floyd_Triangle.java
@@ -0,0 +1,17 @@
+package Pattern;
+import java.util.Scanner;
+public class Floyd_Triangle {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter no of Rows : ");
+        int n =sc.nextInt();
+        int a=1;
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=i; j++){
+                System.out.print(a + " ");
+                a++;
+            }
+            System.out.println();
+        }
+    }
+}
