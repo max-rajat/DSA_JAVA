8dd7adbdb84bcb3284c386722c5898cda81f485d
Rajat Kumar <grajatkumar54321@gmail.com>
2026-07-08 11:03:07 +0530

    numberTriangleVerticallyFliped

---
+++ src/Composite_Patterns/numberTriangleVerticallyFliped.java
@@ -0,0 +1,20 @@
+package Composite_Patterns;
+
+import java.util.Scanner;
+
+public class numberTriangleVerticallyFliped {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("enter no of rows : ");
+        int n = sc.nextInt();
+        for (int i=1;i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.printf(" " + " ");
+            }
+            for(int j=1;j<=i;j++){
+                System.out.printf(j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
