b378003c0ec16bfb233071b6a958951ea13bba5f
Rajat Kumar <grajatkumar54321@gmail.com>
2026-07-08 12:09:20 +0530

    Star Pyramid

---
+++ src/Composite_Patterns/starPyramid.java
@@ -0,0 +1,20 @@
+package Composite_Patterns;
+
+import java.util.Scanner;
+
+public class starPyramid {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("enter no of rows : ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n;i++){
+            for(int j=1; j<=n-i;j++){ //spaces
+                System.out.printf(" " + " ");
+            }
+            for(int j=1; j<=2*i-1;j++){
+                System.out.printf("* ");
+            }
+            System.out.println();
+        }
+    }
+}
