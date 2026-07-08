d7383ad4ba2d36214dc7d3ea5f0b6d787ec7656f
Rajat Kumar <grajatkumar54321@gmail.com>
2026-07-08 10:50:38 +0530

    starTriangleVerticallyFlippedComposite

---
+++ src/Composite_Patterns/starTriangleVerticallyFlippedComposite.java
@@ -0,0 +1,18 @@
+package Composite_Patterns;
+import java.util.Scanner;
+public class starTriangleVerticallyFlippedComposite {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.printf("enter no of rows : ");
+        int n = sc.nextInt();
+        for(int i=1; i<=n;i++){
+            for(int j=1;j<=n-i;j++){
+                System.out.printf(" " + " ");
+            }
+           for(int j=1;j<=i;j++){
+               System.out.print("* ");
+           }System.out.println();
+        }
+
+    }
+}
