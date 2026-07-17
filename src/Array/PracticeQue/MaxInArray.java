df731c55f6c8d1acbf78340bfe20841f7e35fe84
Rajat Kumar <grajatkumar54321@gmail.com>
2026-07-17 11:15:45 +0530

    MaxInArray

---
+++ src/Array/PracticeQue/MaxInArray.java
@@ -0,0 +1,16 @@
+package Array.PracticeQue;
+
+public class MaxInArray {
+    public static void main(String[] args) {
+        int [] arr ={1,9,-200,};
+        int mx=Integer.MIN_VALUE;
+        for(int i=1; i<=arr.length-1;i++){
+//            if(arr[i]>mx){
+//                mx=arr[i];
+//            }
+            mx=Math.max(mx,arr[i]);
+        }
+        System.out.println(mx);
+
+    }
+}
